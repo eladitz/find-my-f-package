53295d40c4a53f4bf1ba60ecf63716369ef920be
Elad Itzkin <eladitzkin@gmail.com>
2022-08-29 13:38:46 +0200

    ver 1

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "find", to: "packages#find"
-  post "received", to: "packages#received"
+  get "received", to: "packages#received"
+  get "profile", to: "users#profile"
   resources :packages, only: [:show, :update]
+  resources :addresses, only: [:index, :show]
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
