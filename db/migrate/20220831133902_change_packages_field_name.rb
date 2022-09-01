26149ff85666f02f8065dcc6f4690e59ef66a3c5
Elad Itzkin <eladitzkin@gmail.com>
2022-09-01 15:38:01 +0200

    colourful

--- db/migrate/20220831133902_change_packages_field_name.rb
+++ db/migrate/20220831133902_change_packages_field_name.rb
@@ -1,6 +1,5 @@
 class ChangePackagesFieldName < ActiveRecord::Migration[7.0]
   def change
-    ## name switched with add user name and change package field
     add_column :packages, :not_register_user_name, :string
   end
 end
