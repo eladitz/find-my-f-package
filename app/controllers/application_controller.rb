8a9d45ab108cca5c2b52aaa49d4afa0ddd80f8ac
Elad Itzkin <eladitzkin@gmail.com>
2022-09-21 22:16:53 +0200

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  protect_from_forgery
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
-  get "about", to: "pages#about"
-  get "find", to: "packages#find"
-  post "find", to: "packages#find"
-  get "received", to: "packages#received"
-  get "packages/:id/availability", to: "packages#availability", as: "availability"
-  get "profile", to: "users#profile"
-  get "print_page/:id", to: "users#print_page", as: "print_page"
-  get "users/adding_profile_form", to: "users#adding_profile_form", as: "adding_profile_form"
-  patch "users/adding_profile_info", to: "users#adding_profile_info", as: "adding_profile_info"
-  get "addresses/new", to: "addresses#new", as: "user_address"
+  scope "(:locale)", :locale => /en|de/ do
 
-  resources :packages
-  resources :addresses
-  resources :chatrooms, only: :show do
-    resources :messages, only: :create
+    devise_for :users
+    root to: "pages#home"
+    get "about", to: "pages#about"
+    get "find", to: "packages#find"
+    post "find", to: "packages#find"
+    get "received", to: "packages#received"
+    get "packages/:id/availability", to: "packages#availability", as: "availability"
+    get "profile", to: "users#profile"
+    get "print_page/:id", to: "users#print_page", as: "print_page"
+    get "users/adding_profile_form", to: "users#adding_profile_form", as: "adding_profile_form"
+    patch "users/adding_profile_info", to: "users#adding_profile_info", as: "adding_profile_info"
+    get "addresses/new", to: "addresses#new", as: "user_address"
+
+    resources :packages
+    resources :addresses
+    resources :chatrooms, only: :show do
+      resources :messages, only: :create
+    end
   end
 end
